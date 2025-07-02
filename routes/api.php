8220b364d95d56f2933d3cedf3c906c7df3ad60e
kouheigit <ookawakouhei777@gmail.com>
2025-07-02 11:35:58 +0900

    Add route api.php

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+Route::post('/login',function(Request $request){
+   $request->validate([
+       'email'=>['required','email'],
+       'password'=>['required'],
+   ]);
+
+   $user = User::where('email',$request->email)->first();
+   if(!$user|| !Hash::check($request->password,$user->password)){
+       throw ValidationException::withMessages([
+           'email'=>['emailが間違っています']
+       ]);
+   }
+    return ['token' => $user->createToken('api-token')->plainTextToken];
+});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/*
+ *
+
+
+
+ */
