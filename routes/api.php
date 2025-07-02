cfef1beacd0fefcf9e83c7273181430c23bf29b5
kouheigit <ookawakouhei777@gmail.com>
2025-07-02 13:42:56 +0900

    Add token-based authentication API using Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -35,10 +35,3 @@ Route::post('/login',function(Request $request){
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-/*
- *
-
-
-
- */
